de353688e1dd7b0f4c2fa052c38b9d59549ac984
Tatyana N <mairon.angband@gmail.com>
2024-09-24 18:10:54 +0300

    add fixtures and test

--- __tests__/rename.test.js
+++ __tests__/rename.test.js
@@ -1,6 +1,22 @@
 import reverse from '../src/rename.js';
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+/*test('reverse', () => {
+    expect(reverse('hello')).toEqual('oll--eh');
+    expect(reverse('')).toEqual('');
+});*/
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 test('reverse', () => {
-    expect(reverse('hello')).toEqual('o-l-l-e-h');
-    expect(reverse('')).toEqual('');
+    const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+    const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8' );
+
+    const html = readFile('text.html');
+    const result = readFile('some.json')
+    expect(reverse(html)).toEqual(result);
 });
